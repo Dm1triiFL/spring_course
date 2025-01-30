38c46dfe7a77b2f4c7ce660e0808f6fd4411d862
dmshu <dmshubin.fl@gmail.com>
2025-01-30 13:32:35 +0800

    Dependency injection (Constructor)

--- src/main/java/org/example/spring_introduction/Dog.java
+++ src/main/java/org/example/spring_introduction/Dog.java
@@ -1,6 +1,11 @@
 package org.example.spring_introduction;
 
 public class Dog implements Pet{
+    public Dog() {
+        System.out.println("Dog bean is created");
+
+    }
+
     @Override
     public void say(){
         System.out.println("Bow-bow");
