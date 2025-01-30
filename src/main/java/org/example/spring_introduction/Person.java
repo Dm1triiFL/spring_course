224ae5b4ffe41d0d42d516455c020a458be369b9
dmshu <dmshubin.fl@gmail.com>
2025-01-30 13:57:28 +0800

    Dependency injection (Setter)

--- src/main/java/org/example/spring_introduction/Person.java
+++ src/main/java/org/example/spring_introduction/Person.java
@@ -3,8 +3,12 @@ package org.example.spring_introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet){
+    public Person(){
         System.out.println("Person bean is created");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class person: set pet");
         this.pet = pet;
     }
 
