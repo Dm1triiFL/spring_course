695fba9154600f8d99ca976abf6d25a896737f10
dmshu <dmshubin.fl@gmail.com>
2025-02-01 21:27:11 +0800

    Dependency injection (Annotatitions Value)

--- src/main/java/org/example/spring_introduction/ConfigWithAnnotations.java
+++ src/main/java/org/example/spring_introduction/ConfigWithAnnotations.java
@@ -12,6 +12,10 @@ public class ConfigWithAnnotations {
         Person person = context.getBean("personBean", Person.class);
         person.callYourPet();
 
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
+
+
         context.close();
     }
 }
--- src/main/java/org/example/spring_introduction/Person.java
+++ src/main/java/org/example/spring_introduction/Person.java
@@ -2,6 +2,7 @@ package org.example.spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
@@ -10,7 +11,9 @@ public class Person {
 //    @Autowired
 //    @Qualifier("dog")
     private Pet pet;
+    @Value("${person.surname}")
     private String surname;
+    @Value("${person.age}")
     private int age;
 
     @Autowired
