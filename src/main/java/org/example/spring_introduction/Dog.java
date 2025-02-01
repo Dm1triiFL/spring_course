608620309fa759f67b83833ea23b97d7e4cb47f7
dmshu <dmshubin.fl@gmail.com>
2025-02-01 14:15:34 +0800

    Dependency injection (init destroy)

--- src/main/java/org/example/spring_introduction/Dog.java
+++ src/main/java/org/example/spring_introduction/Dog.java
@@ -1,10 +1,27 @@
 package org.example.spring_introduction;
 
 public class Dog implements Pet{
+//    private String name;
     public Dog() {
         System.out.println("Dog bean is created");
 
     }
+
+    public void init(){
+        System.out.println("Class Dog init method");
+    }
+
+    public void destroy(){
+        System.out.println("Class Dog destroy method");
+    }
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     @Override
     public void say(){
--- src/main/java/org/example/spring_introduction/Test4.java
+++ src/main/java/org/example/spring_introduction/Test4.java
@@ -5,14 +5,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
-            Dog myDog = (Dog) context.getBean("myPet", Dog.class);
-            Dog yourDog = (Dog) context.getBean("myPet", Dog.class);
-            System.out.println("Переменные ссылаются на один обьект" + (myDog == yourDog));
-
-            System.out.println(myDog);
-            System.out.println(yourDog);
-
-            context.close();
+//            Dog myDog = (Dog) context.getBean("myPet", Dog.class);
+//            myDog.setName("belka");
+//
+//            Dog yourDog = (Dog) context.getBean("myPet", Dog.class);
+//            yourDog.setName("strelka");
+//
+//            System.out.println(myDog.getName());
+//            System.out.println(yourDog.getName());
+//
+//            System.out.println("Переменные ссылаются на один обьект " + (myDog == yourDog));
+//            System.out.println(myDog);
+//            System.out.println(yourDog);
+//
+//            context.close();
 
     }
 }
