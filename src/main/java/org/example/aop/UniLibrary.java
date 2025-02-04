000762998d11c462813313fc2ff139fb10053b2b
dmshu <dmshubin.fl@gmail.com>
2025-02-04 23:09:45 +0800

    AOP Pointcut Combinations

--- src/main/java/org/example/aop/UniLibrary.java
+++ src/main/java/org/example/aop/UniLibrary.java
@@ -10,10 +10,23 @@ public class UniLibrary extends AbstractLibrary {
     }
 
     public void returnBook() {
-        System.out.println("we return a book from UniLibrary");
+        System.out.println("we return a book to UniLibrary");
     }
 
     public void getMagazine() {
         System.out.println("we take magazine from UniLibrary");
     }
+
+    public void returnMagazine() {
+        System.out.println("we return a magazine to UniLibrary");
+    }
+
+    public void addBook() {
+        System.out.println("we add a book to UniLibrary");
+    }
+
+    public void addMagazine() {
+        System.out.println("we add a magazine to UniLibrary");
+    }
+
 }
--- src/main/java/org/example/aop/aspects/LoggingAspectAndSecurityAspect.java
+++ src/main/java/org/example/aop/aspects/LoggingAspectAndSecurityAspect.java
@@ -10,17 +10,70 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAspectAndSecurityAspect {
 
-    @Pointcut("execution(* get*())")
-    public void allGetMethods() {}
-
-    @Before("allGetMethods()")
-    public void beforeGetLoggingAdvice() {
-        System.out.println("beforeGetBookAdvice: try to take book/mag");
+    @Pointcut("execution(* org.example.aop.UniLibrary.*(..))")
+    private void allMethodsFromUniLibrary() {
     }
 
-    @Before("allGetMethods()")
-    public void beforeGetSecurityAdvice(){
-        System.out.println("beforeGetSecurityAdvice: rights verification");
+    @Pointcut("execution(public void org.example.aop.UniLibrary.returnMagazine())")
+    private void returnMagazineFromUniLibrary() {
     }
+
+    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
+    private void allMethodsExceptReturnMagazineFromUniLibrary() {}
+
+    @Before("allMethodsExceptReturnMagazineFromUniLibrary()")
+    public void beforeAllMethodsExceptReturnMagazineFromUniLibrary() {
+        System.out.println("beforeAllMethodsExceptReturnMagazineFromUniLibrary: Log #10");
+    }
+
+
+
+
+
+
+
+
+//    @Pointcut("execution(* org.example.aop.UniLibrary.get*())")
+//    private void allGetMethodsFromUniLibrary() {
+//    }
+//
+//    @Pointcut("execution(* org.example.aop.UniLibrary.return*())")
+//    private void allReturnMethodsFromUniLibrary() {
+//    }
+//
+//    @Pointcut("allGetMethodsFromUniLibrary() || allReturnMethodsFromUniLibrary()")
+//    private void allGetAndReturnMethodsFromUniLibrary() {
+//    }
+//
+//
+//
+//    @Before("allGetMethodsFromUniLibrary()")
+//    public void beforeGetLoggingAdvice() {
+//        System.out.println("beforeGetLoggingAdvice: writing Log #1");
+//    }
+//
+//    @Before("allReturnMethodsFromUniLibrary()")
+//    public void beforeReturnLoggingAdvice() {
+//        System.out.println("beforeGetLoggingAdvice: writing Log #2");
+//    }
+//
+//    @Before("allGetAndReturnMethodsFromUniLibrary()")
+//    public void beforeGetAndReturnLoggingAdvice() {
+//        System.out.println("beforeGetAndReturnLoggingAdvice: writing Log #3");
+//    }
+
+
+//    @Pointcut("execution(* get*())")
+//    public void allGetMethods() {}
+//
+//    @Before("allGetMethods()")
+//    public void beforeGetLoggingAdvice() {
+//        System.out.println("beforeGetBookAdvice: try to take book/mag");
+//    }
+//
+//    @Before("allGetMethods()")
+//    public void beforeGetSecurityAdvice(){
+//        System.out.println("beforeGetSecurityAdvice: rights verification");
+//    }
 
 }
