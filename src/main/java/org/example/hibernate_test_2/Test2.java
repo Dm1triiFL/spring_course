bc0ff9057383e5385ca2eebe68238a136d3d3412
dmshu <dmshubin.fl@gmail.com>
2025-02-09 16:47:24 +0800

    Hibernate One to one (Bi-directional)

--- src/main/java/org/example/hibernate_test_2/Test2.java
+++ src/main/java/org/example/hibernate_test_2/Test2.java
@@ -6,7 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration().
                 configure("hibernate.cfg.xml")
@@ -17,11 +17,18 @@ public class Test1 {
 
 
         try {
-            session.beginTransaction();
-            Detail detail = session.get(Detail.class, 4);
-            System.out.println(detail.getEmployee());
+//            Employee emp = new Employee("Kolya", "ivanov", "HR", 950);
+//            Detail detail = new Detail("new york", "3234234", "midsf@mail.ry");
 
+//            detail.setEmployee(emp);
+//            emp.setEmpDetail(detail);
+
+            session.beginTransaction();
+
+            Detail detail = session.get(Detail.class, 1);
+            detail.getEmployee().setEmpDetail(null);
             session.delete(detail);
+
             session.getTransaction().commit();
             System.out.println("Done");
 
