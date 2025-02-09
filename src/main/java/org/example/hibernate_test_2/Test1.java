0e8f6197c153b70f454cfa4e41db69523b77be8f
dmshu <dmshubin.fl@gmail.com>
2025-02-09 15:14:23 +0800

    Hibernate One to one (Uni-directional)

--- src/main/java/org/example/hibernate_test_2/Test1.java
+++ src/main/java/org/example/hibernate_test_2/Test1.java
@@ -1,31 +1,33 @@
-package org.example.hibernate_test;
+package org.example.hibernate_test_2;
 
-import org.example.hibernate_test.entity.Employee;
+import org.example.hibernate_test_2.entity.Detail;
+import org.example.hibernate_test_2.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test5 {
+public class Test1 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration().
                 configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
                 .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
 
         try {
-            Session session = factory.getCurrentSession();
             session.beginTransaction();
-//            Employee emp = session.get(Employee.class, 1);
-//            session.delete(emp);
 
-            session.createQuery("delete Employee where name = 'Elena'").executeUpdate();
+            Employee emp = session.get(Employee.class, 2);
+            session.delete(emp);
 
             session.getTransaction().commit();
-
             System.out.println("Done");
 
         }
         finally {
+            session.close();
             factory.close();
         }
 
